465d7bc13c23704d74129d431ef4fb6a0fbcc107
Brandon-Guerrero <brandon2001huesos@gmail.com>
2024-12-07 17:13:09 -0500

    primer capitulo de las aventuras de cofla

---
+++ capitulo1/scrpit.js
@@ -0,0 +1,77 @@
+//cofla, roberto y pedro quieren saber que helado mas caro les alcanza con el dinero que tienen
+dineroCofla = prompt("cuanto dinero tienes Cofla?");
+dineroRoberto = prompt("cuanto dinero tienes Roberto?");
+dineroPedro = prompt("cuanto dinero tienes Pedro?");
+//Cofla
+dineroCofla = parseInt(dineroCofla);
+
+if (dineroCofla >= 0.6 && dineroCofla < 1) {
+  alert("Cofla comprate el palito de helado de agua");
+  alert("y te sobra " + (dineroCofla - 0.6));
+}else if (dineroCofla >= 1 && dineroCofla < 1.6) {
+  alert("Coflacomprate el palito de helado de crema");
+  alert("y te sobra " + (dineroCofla - 1));
+}else if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
+  alert("Cofla compra el bombon helado marca heladix");
+  alert("y te sobra " + (dineroCofla - 1.6));
+}else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
+  alert("Cofla compra el bombon helado marca heladovich");
+  alert("y te sobra " + (dineroCofla - 1.7));
+}else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
+  alert("Cofla compra bombon helado marca helardo");
+  alert("y te sobra " + (dineroCofla - 1.8));
+}else if (dineroCofla >= 2.9) {
+  alert("Cofla compra potecito de helado con confites o el pote de 1/4 KG");
+  alert("y te sobra " + (dineroCofla - 2.9));
+}else {
+  alert("lo sineto no te alcanza para nada");
+}
+//Roberto
+dineroRoberto = parseInt(dineroRoberto);
+
+if (dineroRoberto >= 0.6 && dineroRoberto < 1) {
+  alert("Roberto comprate el palito de helado de agua");
+  alert("y te sobra " + (dineroRoberto - 0.6));
+}else if (dineroRoberto >= 1 && dineroRoberto < 1.6) {
+  alert("Roberto comprate el palito de helado de crema");
+  alert("y te sobra " + (dineroRoberto - 1));
+}else if (dineroRoberto >= 1.6 && dineroRoberto < 1.7) {
+  alert("Roberto compra el bombon helado marca heladix");
+  alert("y te sobra " + (dineroRoberto - 1.6));
+}else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8) {
+  alert("Roberto compra el bombon helado marca heladovich");
+  alert("y te sobra " + (dineroRoberto - 1.7));
+}else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9) {
+  alert("Roberto compra bombon helado marca helardo");
+  alert("y te sobra " + (dineroRoberto - 1.8));
+}else if (dineroRoberto >= 2.9) {
+  alert("Roberto compra potecito de helado con confites  o el pote de 1/4 KG");
+  alert("y te sobra " + (dineroRoberto - 2.9));
+}else {
+  alert("lo sineto no te alcanza para nada")
+}
+//Pedro
+dineroPedro = parseInt(dineroPedro);
+
+if (dineroPedro >= 0.6 && dineroPedro < 1) {
+  alert("Pedro comprate el palito de helado de agua");
+  alert("y te sobra " + (dineroPedro - 0.6));
+}else if (dineroPedro >= 1 && dineroPedro < 1.6) {
+  alert("Pedro comprate el palito de helado de crema");
+  alert("y te sobra " + (dineroPedro - 1));
+}else if (dineroPedro >= 1.6 && dineroPedro < 1.7) {
+  alert("Pedro compra el bombon helado marca heladix");
+  alert("y te sobra " + (dineroPedro - 1.6));
+}else if (dineroPedro >= 1.7 && dineroPedro < 1.8) {
+  alert("Pedro compra el bombon helado marca heladovich");
+  alert("y te sobra " + (dineroPedro - 1.7));
+}else if (dineroPedro >= 1.8 && dineroPedro < 2.9) {
+  alert("Pedro compra bombon helado marca helardo");
+  alert("y te sobra " + (dineroPedro - 1.8));
+}else if (dineroPedro >= 2.9) {
+  alert("Pedro compra potecito de helado con confites  o el pote de 1/4 KG");
+  alert("y te sobra " + (dineroPedro - 2.9));
+}else {
+  alert("lo sineto no te alcanza para nada")
+}
+//cofla quiere saber cual es su vuelto 
